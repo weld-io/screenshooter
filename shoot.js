942b8b4def7a8356c5f8acb4a0c80e9c2d5b8d29
Henric Malmberg <henric.malmberg@magine.com>
2014-04-09 14:22:36 +0200

    phantomjs is back

--- shoot.js
+++ shoot.js
@@ -1,45 +1,41 @@
 var http = require('http');
-//var phantom=require('node-phantom-simple');
+var phantom=require('node-phantom-simple');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-  // phantom.create(function(err,ph) {
-  //   return ph.createPage(function(err, page) {
-  //     return page.open(req.url.slice(1), function(err, status) {
-  //       //Wait for a bit for AJAX content to load on the page. Better solution?
-  //       setTimeout(function() {
-  //         page.renderBase64("PNG", function(error, result){
-  //           var imageBuffer = new Buffer(result, 'base64');
-  //           res.writeHead(200, {
-  //             'Content-Type': 'image/png',
-  //             'Content-Length': imageBuffer.length});
-  //           res.end(imageBuffer);
-  //           ph.exit()
-  //         });
-  //       }, 2000);
-  //     });
-  //   });
-  // });
-  // phantom.create(function(err,ph) {
-  //   return ph.createPage(function(err, page) {
-  //     return page.open(req.url.slice(1), function(err, status) {
-  //       //Wait for a bit for AJAX content to load on the page. Better solution?
-  //       setTimeout(function() {
-  //         page.renderBase64("PNG", function(error, result){
-  //           var imageBuffer = new Buffer(result, 'base64');
-  //           res.writeHead(200, {
-  //             'Content-Type': 'image/png',
-  //             'Content-Length': imageBuffer.length});
-  //           res.end(imageBuffer);
-  //           ph.exit()
-  //         });
-  //       }, 2000);
-  //     });
-  //   });
-  // });
+  phantom.create(function(err,ph) {
+    return ph.createPage(function(err, page) {
+      return page.open(req.url.slice(1), function(err, status) {
+        //Wait for a bit for AJAX content to load on the page. Better solution?
+        setTimeout(function() {
+          page.renderBase64("PNG", function(error, result){
+            var imageBuffer = new Buffer(result, 'base64');
+            res.writeHead(200, {
+              'Content-Type': 'image/png',
+              'Content-Length': imageBuffer.length});
+            res.end(imageBuffer);
+            ph.exit()
+          });
+        }, 2000);
+      });
+    });
+  });
+  phantom.create(function(err,ph) {
+    return ph.createPage(function(err, page) {
+      return page.open(req.url.slice(1), function(err, status) {
+        //Wait for a bit for AJAX content to load on the page. Better solution?
+        setTimeout(function() {
+          page.renderBase64("PNG", function(error, result){
+            var imageBuffer = new Buffer(result, 'base64');
+            res.writeHead(200, {
+              'Content-Type': 'image/png',
+              'Content-Length': imageBuffer.length});
+            res.end(imageBuffer);
+            ph.exit()
+          });
+        }, 2000);
+      });
+    });
+  });
 }).listen(process.env.PORT || 1337, function(){
-  console.log('Server running at port 1337');
+  console.log('running');
 });
-
-
