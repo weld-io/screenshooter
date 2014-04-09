a4d82015d275cc95e4f9e877cb2dcb44cbe9bdcc
Henric Malmberg <henric.malmberg@magine.com>
2014-04-09 11:43:29 +0200

    change to use environment port

--- shoot.js
+++ shoot.js
@@ -19,6 +19,6 @@ http.createServer(function (req, res) {
       });
     });
   });
-}).listen(1337, '127.0.0.1');
+}).listen(process.env.PORT || 1337, '127.0.0.1');
 
 console.log('Server running at port 1337');
