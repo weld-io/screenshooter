f367c003a04bea6a470461b79d7b1db2c176b2b2
Henric Malmberg <henric.malmberg@jagodu.com>
2014-07-27 12:42:55 +0200

    Request only one page at a time to not run out of memory

--- shoot.js
+++ shoot.js
@@ -1,24 +1,45 @@
 var http = require('http');
-var phantom=require('node-phantom-simple');
+var phantom = require('node-phantom-simple');
 
+var processing = false;
+var requests = [];
+
+// Request the current page and send back as png
+// Then process next request in queue if there is one
+var process = function(req, res){
+	if(req && res){
+		processing = true;
+		phantom.create(function(err,ph) {
+			return ph.createPage(function(err, page) {
+				return page.open(req.url.slice(1), function(err, status) {
+					//Wait for a bit for AJAX content to load on the page. Better solution?
+					setTimeout(function() {
+						page.renderBase64("PNG", function(error, result){
+							var imageBuffer = new Buffer(result, 'base64');
+							res.writeHead(200, {
+								'Content-Type': 'image/png',
+								'Content-Length': imageBuffer.length});
+							res.end(imageBuffer);
+							ph.exit()
+							process.apply(null, requests.shift());
+						});
+					}, 1000);
+				});
+			});
+		});
+	} else {
+		processing = false;
+	}
+};
+
+// Process the incoming request if not already processing
+// Else put into queue
 http.createServer(function (req, res) {
-  phantom.create(function(err,ph) {
-    return ph.createPage(function(err, page) {
-      return page.open(req.url.slice(1), function(err, status) {
-        //Wait for a bit for AJAX content to load on the page. Better solution?
-        setTimeout(function() {
-          page.renderBase64("PNG", function(error, result){
-            var imageBuffer = new Buffer(result, 'base64');
-            res.writeHead(200, {
-              'Content-Type': 'image/png',
-              'Content-Length': imageBuffer.length});
-            res.end(imageBuffer);
-            ph.exit()
-          });
-        }, 1000);
-      });
-    });
-  });
+	if(!processing){
+		process(req, res);
+	} else {
+		requests.push([req, res]);
+	}
 }).listen(process.env.PORT || 1337, function(){
-  console.log('running');
+	console.log('running');
 });
