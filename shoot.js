d542d509ce010b5150925c27a5b99a58efe91cf0
Henric Malmberg <henric@weld.io>
2014-11-06 14:00:26 +0100

    240px wide files in jpg format

--- shoot.js
+++ shoot.js
@@ -1,8 +1,30 @@
 var http = require('http');
 var phantom = require('node-phantom-simple');
+var lwip = require('lwip');
 
 var processing = false;
 var requests = [];
+
+var processImage = function(res, result, ph){
+	var imageBuffer = new Buffer(result, 'base64');
+	lwip.open(imageBuffer, 'jpg', function(err, image){
+		if (image) {
+			image.scale(0.234375, function(err, image){
+				image.toBuffer('jpg', function(err, imageBuffer){
+					res.writeHead(200, {
+						'Content-Type': 'image/jpg',
+						'Content-Length': imageBuffer.length});
+					res.end(imageBuffer);
+					ph.exit()
+					processRequest.apply(null, requests.shift());
+				});
+			});
+		} else {
+			console.log(err);
+			res.send(500);
+		}
+	});
+}
 
 // Request the current page and send back as png
 // Then process next request in queue if there is one
@@ -16,18 +38,15 @@ var processRequest = function(req, res){
 					height: 1024
 				});
 				return page.open(req.url.slice(1), function(err, status) {
-					//Wait for a bit for AJAX content to load on the page. Better solution?
-					setTimeout(function() {
-						page.renderBase64("PNG", function(error, result){
-							var imageBuffer = new Buffer(result, 'base64');
-							res.writeHead(200, {
-								'Content-Type': 'image/png',
-								'Content-Length': imageBuffer.length});
-							res.end(imageBuffer);
-							ph.exit()
-							processRequest.apply(null, requests.shift());
-						});
-					}, 1000);
+					page.renderBase64("JPEG", function(error, result){
+						if (result) {
+							processImage(res, result, ph);
+						} else {
+							console.log(error);
+							res.send(500);
+						}
+					});
+
 				});
 			}, {parameters: {'ignore-ssl-errors': 'yes'}});
 		});
