73f16cc9f09ebdedf7fd03666931bfab5a58ab66
Tom Söderlund <tom@tomsoderlund.com>
2015-12-29 15:12:15 +0100

    Work queue is back

--- shoot.js
+++ shoot.js
@@ -15,6 +15,11 @@ var defaultOptions = {
 	browserHeight: 1024,
 };
 
+var MAX_PARALLELL_JOBS = 3;
+var requestsBeingProcessed = 0;
+var requestQueue = [];
+var workingOnQueue = false;
+
 var formatImage = function(imageData, imageOptions, callback){
 	var imageBuffer = new Buffer(imageData, 'base64');
 	// Resize options: %, @, !, < or > see http://aheckmann.github.io/gm/docs.html
@@ -31,7 +36,7 @@ var renderUrlToImage = function (url, imageOptions, callback) {
 	var lastTime = Date.now();
 
 	var logTimestamp = function (msg) {
-		console.log(msg, (Date.now() - lastTime));
+		console.log('%s %s (%d)', msg, url, (Date.now() - lastTime));
 		lastTime = Date.now();
 	};
 
@@ -76,15 +81,15 @@ var renderUrlToImage = function (url, imageOptions, callback) {
 	callback);
 };
 
-// Process the incoming request if not already processing
-// Else put into queue
-var onIncomingHTTPRequest = function (req, res) {
-	console.log('Incoming request:', req.url);
+// Take a request object and work on it
+var processHTTPRequest = function (req, res, callback) {
 	var pageURL = req.url.slice(1);
 	var imageOptions = _.merge({}, defaultOptions);
 	_.merge(imageOptions, url.parse(req.url, true).query);
+	requestsBeingProcessed++;
 
 	renderUrlToImage(pageURL, imageOptions, function (err, imageBuffer) {
+		requestsBeingProcessed--;
 		if (!err) {
 			res.writeHead(200, {
 				'Content-Type': 'image/' + imageOptions.imageFormat,
@@ -93,13 +98,54 @@ var onIncomingHTTPRequest = function (req, res) {
 			res.end(imageBuffer);
 		}
 		else {
-			console.log('renderUrlToImage error:', error);
-			res.send(500);
+			console.log('Image render error:', err);
+			res.json(500);
 		}
+		if (callback) callback(err);
 	});
 };
 
+// Take a request object and work on it
+var addToRequestQueue = function (req, res) {
+	requestQueue.push({ req: req, res: res });
+	// Start working on queue if not doing it already
+	if (!workingOnQueue) {
+		workingOnQueue = true;
+		async.whilst(
+			function () { return requestQueue.length > 0; },
+			function (callback) {
+				console.log('Working on: %d, in queue: %d', requestsBeingProcessed, requestQueue.length);
+				var firstRequest = requestQueue.shift();
+				processHTTPRequest(firstRequest.req, firstRequest.res);
+				callback(null);
+			},
+			function (err, results) {
+				workingOnQueue = false;
+			}
+		);		
+	}
+}
+
+// Process the incoming request if not already processing
+// Else put into queue
+var onIncomingHTTPRequest = function (req, res) {
+	console.log('Incoming request:', req.url);
+	if (requestsBeingProcessed < MAX_PARALLELL_JOBS) {
+		processHTTPRequest(req, res); // Process immediately
+	}
+	else {
+		addToRequestQueue(req, res); // Add to queue
+	}
+};
+
 // Start server
-http.createServer(onIncomingHTTPRequest).listen(process.env.PORT || 1337, function(){
-	console.log('Screenshooter running on http://localhost:' + (process.env.PORT || 1337));
+var serverPort = process.env.PORT || 1337;
+var server = http.createServer(onIncomingHTTPRequest);
+
+server.on('close', function () {
+	console.log('Closed');
+});
+
+server.listen(serverPort, function () {
+	console.log('Screenshooter running on http://localhost:' + serverPort);
 });
