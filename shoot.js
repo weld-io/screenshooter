1a8408152f2de8ddd8fb0038241ec50da92de340
Henric Malmberg <henric@weld.io>
2017-12-12 15:56:20 +0100

    chromium based puppeteer instead of phantomjs

--- shoot.js
+++ shoot.js
@@ -1,16 +1,14 @@
 #!/usr/bin/env node
 
-'use strict';
+const _ = require('lodash');
+const async = require('async');
+const http = require('http');
+const puppeteer = require('puppeteer');
 
-var _ = require('lodash');
-var async = require('async');
-var http = require('http');
-var phantom = require('node-phantom-simple');
-var gm = require('gm');
-var url = require('url');
-var fs = require('fs');
+const url = require('url');
+const fs = require('fs');
 
-var defaultOptions = {
+const defaultOptions = {
 	imageWidth: 240,
 	imageHeight: 240,
 	imageFormat: 'jpg',
@@ -20,121 +18,67 @@ var defaultOptions = {
 	trim: false,
 };
 
-var MAX_PARALLELL_JOBS = (process.env['MAX_PARALLELL_JOBS'] ? parseInt(process.env['MAX_PARALLELL_JOBS']) : 3);
-var VERBOSE_LOGGING = (process.env['VERBOSE_LOGGING'] === 'false' ? false : true);
-var requestsBeingProcessed = 0;
-var requestQueue = [];
-var workingOnQueue = false;
+const MAX_PARALLELL_JOBS = (process.env['MAX_PARALLELL_JOBS'] ? parseInt(process.env['MAX_PARALLELL_JOBS']) : 3);
+const VERBOSE_LOGGING = (process.env['VERBOSE_LOGGING'] === 'false' ? false : true);
+let requestsBeingProcessed = 0;
+const requestQueue = [];
+let workingOnQueue = false;
 
-var formatImage = function(imageData, imageOptions, callback){
-	var imageBuffer = new Buffer(imageData, 'base64');
-	// GraphicsMagick options: see http://aheckmann.github.io/gm/docs.html
-	var imageObj = gm(imageBuffer, 'image.' + imageOptions.imageFormat);
-	if (imageOptions.trim) {
-		// Trim: for symbols etc
-		imageObj.trim();
-		imageObj.gravity('Center');
-		imageObj.resize(imageOptions.imageWidth, imageOptions.imageHeight, '>');
-	}
-	else {
-		// No trim: for screens
-		imageObj.gravity(imageOptions.gravity);
-		imageObj.resize(imageOptions.imageWidth, imageOptions.imageHeight, '^');
-		imageObj.crop(imageOptions.imageWidth, imageOptions.imageHeight);
-	}
-	imageObj.toBuffer(imageOptions.imageFormat.toUpperCase(), callback);
-}
 
 // Take URL, deliver image buffer
-var renderUrlToImage = function (url, imageOptions, callback) {
-	var lastTime = Date.now();
+const renderUrlToImage = (url, imageOptions, callback) => {
 
-	var logTimestamp = function (msg) {
-		if (VERBOSE_LOGGING)
-			console.log('%s: %s (%d)', msg, url, (Date.now() - lastTime));
-		lastTime = Date.now();
-	};
+	let renderImageFormat;
+	if (imageOptions.imageFormat === 'jpg') {
+		renderImageFormat = 'jpeg';
+	}
+	else if (imageOptions.imageFormat) {
+		renderImageFormat = imageOptions.imageFormat.toLowerCase();
+	}
 
-	var thePhantomInstance;
-	async.waterfall([
-		// Init PhantomJS every request - saves RAM?
-		phantom.create,
-		// Create page
-		function (ph, cbWaterfall) {
-			logTimestamp('Create page');
-			thePhantomInstance = ph;
-			thePhantomInstance.createPage(cbWaterfall, {parameters: {'ignore-ssl-errors': 'yes'}});
-		},
-		// Open URL
-		function (page, cbWaterfall) {
-			logTimestamp('Open URL');
-			page.set('resourceTimeout', 3000); // 3 seconds
-			page.set('viewportSize', {
-				width: imageOptions.browserWidth,
-				height: imageOptions.browserHeight
-			});
-			page.open(url, function (err, status) {
-				cbWaterfall(err, page);
-			});
-		},
-		// Render page to image
-		function (page, cbWaterfall) {
-			logTimestamp('Render page');
-			try {
-				var renderImageFormat;
-				if (imageOptions.imageFormat === 'jpg') {
-					renderImageFormat = 'JPEG';
-				}
-				else if (imageOptions.imageFormat) {
-					renderImageFormat = imageOptions.imageFormat.toUpperCase();
-				}
-				else {
-					renderImageFormat = 'PNG';
-				}
-				page.renderBase64(renderImageFormat, cbWaterfall);
-			}
-			catch (err) {
-				console.log('Error renderImageFormat', err, imageOptions);
-				cbWaterfall(err);
-			}
-		},
-		// Format image
-		function (imageData, cbWaterfall) {
-			logTimestamp('Format image');
-			formatImage(imageData, imageOptions, cbWaterfall);
-		},
-	],
-	function (err, results) {
-		thePhantomInstance.exit();
-		callback(err, results);
+	(async () => {
+		const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+		const page = await browser.newPage();
+		page.setViewport({
+			width: imageOptions.browserWidth,
+			height: imageOptions.browserHeight
+		});
+		await page.goto(url, {waitUntil: 'networkidle2'});
+		const screenshot = await page.screenshot({
+			type: renderImageFormat
+		});
+		browser.close();
+		return screenshot;
+	})().then(screenshot => {
+		callback(null, screenshot);
 	});
 };
 
 // Save image to disk
-var saveImageBufferToDisk = function (fileName, imageBuffer, callback) {
+const saveImageBufferToDisk = (fileName, imageBuffer, callback) => {
 	console.log('Saving to disk:', fileName);
 	fs.writeFile(fileName, imageBuffer, 'binary', callback);
 };
 
 // Take a request object and work on it
-var processHTTPRequest = function (req, res, callback) {
-	var pageURL = req.url.slice(1);
-	var imageOptions = _.merge({}, defaultOptions);
+const processHTTPRequest = (req, res, callback) => {
+	const pageURL = req.url.slice(1);
+	const imageOptions = _.merge({}, defaultOptions);
 	_.merge(imageOptions, url.parse(req.url, true).query);
 
-	if (pageURL.indexOf('http') === -1) {
+	if (!pageURL.includes('http')) {
 		// No URL
 		if (callback) callback('Not valid URL');
 	}
 	else {
 		requestsBeingProcessed++;
 		console.log('Working on: %s (total %d)', pageURL, requestsBeingProcessed);
-		renderUrlToImage(pageURL, imageOptions, function (err, imageBuffer) {
+		renderUrlToImage(pageURL, imageOptions, (err, imageBuffer) => {
 			requestsBeingProcessed--;
 			console.log('Done with: %s (total %d)', pageURL, requestsBeingProcessed);
 			if (!err) {
 				res.writeHead(200, {
-					'Content-Type': 'image/' + imageOptions.imageFormat,
+					'Content-Type': `image/${imageOptions.imageFormat}`,
 					'Content-Length': imageBuffer.length,
 					'Cache-Control': 'public, max-age=31536000'});
 				res.end(imageBuffer);
@@ -153,31 +97,31 @@ var processHTTPRequest = function (req, res, callback) {
 };
 
 // Take a request object and work on it
-var addToRequestQueue = function (req, res) {
-	requestQueue.push({ req: req, res: res });
+const addToRequestQueue = (req, res) => {
+	requestQueue.push({ req, res });
 	// Start working on queue if not doing it already
 	if (!workingOnQueue) {
 		workingOnQueue = true;
 		// While: 1) there is spare capacity, and 2) there is work in the queue
 		async.whilst(
-			function () { return requestsBeingProcessed < MAX_PARALLELL_JOBS && requestQueue.length > 0; },
-			function (callback) {
+			() => requestsBeingProcessed < MAX_PARALLELL_JOBS && requestQueue.length > 0,
+			callback => {
 				console.log('Working on: %d, in queue: %d', requestsBeingProcessed, requestQueue.length);
-				var firstRequest = requestQueue.shift();
+				const firstRequest = requestQueue.shift();
 				processHTTPRequest(firstRequest.req, firstRequest.res);
 				callback(null);
 			},
-			function (err, results) {
+			(err, results) => {
 				console.log('Done. Working on: %d, in queue: %d', requestsBeingProcessed, requestQueue.length);
 				workingOnQueue = false;
 			}
 		);		
 	}
-}
+};
 
 // Process the incoming request if not already processing
 // Else put into queue
-var onIncomingHTTPRequest = function (req, res) {
+const onIncomingHTTPRequest = (req, res) => {
 	console.log('Incoming request:', req.url);
 	if (requestsBeingProcessed < MAX_PARALLELL_JOBS) {
 		processHTTPRequest(req, res); // Process immediately
@@ -187,34 +131,31 @@ var onIncomingHTTPRequest = function (req, res) {
 	}
 };
 
-var processCommandLine = function () {
-	var imageOptions = _.merge({}, defaultOptions);
+const processCommandLine = () => {
+	const imageOptions = _.merge({}, defaultOptions);
 	async.waterfall([
-			// Process arguments
-			function (cbWaterfall) {
-				for (var i = 2; i < process.argv.length; i++) {
-					var arg = process.argv[i];
-					if (arg.indexOf('http') !== -1) {
+			cbWaterfall => {
+				for (let i = 2; i < process.argv.length; i++) {
+					const arg = process.argv[i];
+					if (arg.includes('http')) {
 						imageOptions.url = arg;
 					}
-					else if (arg.indexOf('=') !== -1) {
-						var param = arg.split('=');
+					else if (arg.includes('=')) {
+						const param = arg.split('=');
 						imageOptions[param[0]] = param[1];
 					}
-					else if (arg.indexOf('.') !== -1) {
+					else if (arg.includes('.')) {
 						imageOptions.fileName = arg;
 					}
 				};
-				imageOptions.fileName = imageOptions.fileName || 'screenshot.' + imageOptions.imageFormat;
+				imageOptions.fileName = imageOptions.fileName || `screenshot.${imageOptions.imageFormat}`;
 				cbWaterfall(null, imageOptions);
 			},
-			// Render page
-			function (imageOptions, cbWaterfall) {
+			(imageOptions, cbWaterfall) => {
 				console.log('Render URL to image', imageOptions);
 				renderUrlToImage(imageOptions.url, imageOptions, cbWaterfall);
 			},
-			// Save to disk
-			function (imageBuffer, cbWaterfall) {
+			(imageBuffer, cbWaterfall) => {
 				saveImageBufferToDisk(imageOptions.fileName, imageBuffer, cbWaterfall);
 			},
 		]
@@ -222,12 +163,12 @@ var processCommandLine = function () {
 };
 
 // Start server
-var startWebServer = function () {
-	var serverPort = process.env.PORT || 1337;
-	var server = http.createServer(onIncomingHTTPRequest);
+const startWebServer = () => {
+	const serverPort = process.env.PORT || 1337;
+	const server = http.createServer(onIncomingHTTPRequest);
 
-	server.listen(serverPort, function () {
-		console.log('Screenshooter service running on http://localhost:' + serverPort);
+	server.listen(serverPort, () => {
+		console.log(`Screenshooter service running on http://localhost:${serverPort}`);
 	});
 };
 
